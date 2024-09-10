fcf4ff9158dafe0854dffa0063fde35262049a14
savajarpit <158606373+savajarpit@users.noreply.github.com>
2024-09-11 00:46:16 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -22,7 +22,7 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname,"./client/build")))
+app.use(express.static(path.join(__dirname,"./client/dist")))
 
 //routes
 app.use("/api/v1/auth",authRoute)
@@ -32,7 +32,7 @@ app.use("/api/v1/payment",productRoutes)
 
 // rest api
 app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname,'.client/build/index.html'))
+    res.sendFile(path.join(__dirname,'.client/dist/index.html'))
 })
 
 // PORT
